184a4485dc125a1d6b49d90e56865b5073ff9b9b
Krissy-jba <krissy@prideandjoy.org>
2018-05-03 08:02:17 +1200

    results

--- exercise1/pages/006.js
+++ exercise1/pages/006.js
@@ -12,18 +12,18 @@ pageComponentry = {
     methods: {
         
     },
-    ready: function() {
+   ready: function() {
         var self = this;
-        var firstExerciseId = this.exercises[0].id;
-        $.get( "/api/v2/course/96/data?exercise_id=" + firstExerciseId, function( data ) {
-            var week1Questions = JSON.parse(data.CourseData.questions);
+        var firstExerciseId = this.courseData[Object.keys(this.courseData)[0]];
+        
+        console.log(firstExerciseId);
+
+            var week1Questions = JSON.parse(firstExerciseId.questions);
             for (var i = 0; i < week1Questions.length; i++) {
-                console.log(week1Questions[i].selected);
                 if(week1Questions[i].selected){
                     self.questions.push(week1Questions[i]);
                 }
             }
-        });
-
+       
     }
 }
