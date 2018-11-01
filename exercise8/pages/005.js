5934cf359249fc7abd433fb31d33be1d19663c87
JosephJvB <joevanbo@pm.me>
2018-11-02 11:43:08 +1300

    prog(x8p5): pdf hosted in slide page rather than in new tab

--- exercise8/pages/005.js
+++ exercise8/pages/005.js
@@ -2,15 +2,17 @@ pageComponentry = {
     data: function() {
         return {
             pageTitle: 'Health and Fitness',
-            image: '5.png'
-
+            image: '5.png',
+            activePdf: false
         }
     },
     computed: {
 
     },
     methods: {   
-
+        showPdf: function (pdfType) {
+            this.activePdf = pdfType
+        }
     },
     events:{
         
